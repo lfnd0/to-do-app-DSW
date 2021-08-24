c5266d37187e2a5da2f22afed5bf2073d7f8944a
luizfernando <luiz-fsilva37@hotmail.com>
2021-08-23 22:23:21 -0300

    Adição todo project

---
+++ todo/urls.py
@@ -0,0 +1,8 @@
+import tasks
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('tasks.urls')),
+    path('admin/', admin.site.urls),
+]
