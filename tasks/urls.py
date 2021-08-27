46ff85f9b24ba7437d944c34b6cb9a2b55a343f6
luizfernando <luiz-fsilva37@hotmail.com>
2021-08-27 11:20:29 -0300

    Atualização tasks app

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,11 +4,11 @@ from .views import TasksListView, TaskCreateView, update_description_task, updat
 urlpatterns = [
     path('', TasksListView.as_view(), name='list-tasks'),
     path('create-task/', TaskCreateView.as_view(), name='create-task'),
-    path('update-description-task/<int:pk>',
+    path('update-description-task/<int:id>',
          update_description_task, name='update-description-task'),
-    path('update-status-task/<int:pk>/',
+    path('update-status-task/<int:id>/',
          update_status_task, name='update-status-task'),
-    path('delete-task/<int:pk>/', delete_task, name='delete-task'),
+    path('delete-task/<int:id>/', delete_task, name='delete-task'),
     path('completed-tasks/', completed_tasks, name='completed-tasks'),
     path('pendent-tasks/', pendent_tasks, name='pendent-tasks'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -21,25 +21,23 @@ class TaskCreateView(CreateView):
     success_url = reverse_lazy('list-tasks')
 
 
-def update_description_task(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    form = TaskModelForm(instance=task)
+def update_description_task(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskModelForm(request.POST or None, instance=task)
 
-    if(request.method == 'POST'):
-        form = TaskModelForm(request.POST, instance=task)
+    if(task.status == 'Pendente'):
         if(form.is_valid()):
-            task.status = 'Pendente'
             task.creation_date = timezone.now()
             task.save()
             return redirect('list-tasks')
         else:
             return render(request, 'tasks/update-description-task.html', {'form': form, 'task': task})
     else:
-        return render(request, 'tasks/update-description-task.html', {'form': form, 'task': task})
+        return redirect('list-tasks')
 
 
-def update_status_task(request, pk):
-    task = get_object_or_404(Task, pk=pk)
+def update_status_task(request, id):
+    task = get_object_or_404(Task, pk=id)
 
     if(task.status == 'Pendente'):
         task.status = 'Concluída'
@@ -49,8 +47,8 @@ def update_status_task(request, pk):
     return redirect('list-tasks')
 
 
-def delete_task(request, pk):
-    task = get_object_or_404(Task, pk=pk)
+def delete_task(request, id):
+    task = get_object_or_404(Task, pk=id)
     task.delete()
     return redirect('list-tasks')
 
